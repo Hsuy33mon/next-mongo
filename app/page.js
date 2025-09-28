eeb7e2c794082aee6d73e9d9d1d1b0d18722fd2f
Hsuy33mon <Hannah682682@gmail.com>
2025-09-28 23:33:08 +0700

    initial

---
+++ app/page.js
@@ -0,0 +1,23 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+
+export default function BoxBasic() {
+  return (
+    <main>
+      <Box component="section" className="border border-gray-800 m-5 text-center">
+        <h1 className="text-3xl text-violet-950">Stock Management v1.0</h1>
+        <ul>
+          <li><a href="/product">Products</a></li>
+          <li><a href="/category">Category</a></li>
+        </ul>
+        
+      </Box>
+    </main>
+  );
+}
+
+// hsu_db_user
+// 7ym9GKbVemEQTyEa
+
+// mongodb+srv://hsu_db_user:7ym9GKbVemEQTyEa@stock.hdb2ft0.mongodb.net/
