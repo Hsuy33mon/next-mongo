bd4f65a344acce8087473bb2caf37382d0fb6b46
Hsuy33mon <Hannah682682@gmail.com>
2025-09-30 23:38:55 +0700

    correct path

--- app/page.js
+++ app/page.js
@@ -8,16 +8,11 @@ export default function BoxBasic() {
       <Box component="section" className="border border-gray-800 m-5 text-center">
         <h1 className="text-3xl text-violet-950">Stock Management v1.0</h1>
         <ul>
-          <li><a href="/product">Products</a></li>
-          <li><a href="/category">Category</a></li>
+          <li><a href="/stock/product">Products</a></li>
+          <li><a href="/stock/category">Category</a></li>
         </ul>
         
       </Box>
     </main>
   );
 }
-
-// hsu_db_user
-// 7ym9GKbVemEQTyEa
-
-// mongodb+srv://hsu_db_user:7ym9GKbVemEQTyEa@stock.hdb2ft0.mongodb.net/
